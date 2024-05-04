bb0e6b7021f579d0d5048a042982e997b0243bcf
VictorAlencar00 <victordesousaa@gmail.com>
2024-05-04 20:16:58 -0300

    feat: category form structure

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,9 +1,16 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { MatTableModule, MatTable } from '@angular/material/table';
+import { MatTableModule, MatTable, MatTableDataSource } from '@angular/material/table';
 import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
 import { MatSortModule, MatSort } from '@angular/material/sort';
-import { CategoriesDataSource, CategoriesItem } from './categories-datasource';
 import { MatCardModule } from '@angular/material/card';
+import { lastValueFrom } from 'rxjs';
+
+import { CategoriesDataSource, CategoriesItem } from './categories-datasource';
+import { Category} from './category.dto';
+import { CategoryService } from './category.service';
+import { CategoryFormComponent } from './form/form.component';
+
+
 
 @Component({
   selector: 'app-categories',
@@ -12,23 +19,31 @@ import { MatCardModule } from '@angular/material/card';
     .full-width-table {
       width: 100%;
     }
-
   `,
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule, MatSortModule, MatCardModule]
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule, MatCardModule, CategoryFormComponent]
 })
 export class CategoriesComponent implements AfterViewInit {
+
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   @ViewChild(MatTable) table!: MatTable<CategoriesItem>;
-  dataSource = new CategoriesDataSource();
+  dataSource = new MatTableDataSource<Category>();
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = ['id', 'name', 'description'];
+
+  constructor(private categoryService: CategoryService) {}
 
   ngAfterViewInit(): void {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
-    this.table.dataSource = this.dataSource;
+    this.loadCategories()
   }
+
+  async loadCategories(): Promise<void> {
+    const categories = await lastValueFrom(this.categoryService.getAll())
+    this.dataSource = new MatTableDataSource(categories)
+    this.table.dataSource = this.dataSource
+    this.dataSource.sort = this.sort
+    this.dataSource.paginator = this.paginator
+    }
 }
---
+++ src/app/categories/form/form.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'category-form',
+  standalone: true,
+  imports: [ ReactiveFormsModule, MatButtonModule, MatCardModule, MatInputModule],
+  templateUrl: './form.component.html',
+  styles: ``
+})
+export class CategoryFormComponent {
+  constructor(private fb: FormBuilder) {}
+  categoryForm = this.fb.group({
+    id: [null],
+    name: ['', Validators.required],
+    description: ['', Validators.required]
+  })
+}
